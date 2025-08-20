f0836831a888f6050e2292df52d790d97a9f1220
Jacob Anderson <jcnderson2@gmail.com>
2025-08-20 11:01:14 -0400

    added hub vnet

--- main.tf
+++ main.tf
@@ -6,9 +6,13 @@ module "rg_main" {
   source   = "./modules/prod-rg-1"
   name     = "prod-rg-1"
   location = "eastus"
+}
 
-  tags = {
-    Env        = "dev"
-    managed-by = "terraform"
-  }
+module "hub_vnet" {
+  source         = "./modules/vnet-hub"
+  location       = var.location
+  vnet_name      = var.hub_vnet_name
+  address_space  = var.hub_vnet_cidr
+  subnets        = var.hub_subnets
+  resource_group = module.rg_main.name
 }
